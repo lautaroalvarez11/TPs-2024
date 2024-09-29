01f9ad285cc9d8f0dc84c2c3f5d223b37b15e400
lautaroalvarez11 <lealvarez485@comunidad.unnoba.edu.ar>
2024-09-29 01:09:56 -0300

    .

--- TP2/Ej1.cpp
+++ TP2/Ej1.cpp
@@ -9,54 +9,99 @@ El programa debe solicitar al usuario que ingrese, por teclado, nombres de difer
 Por otra parte, se debe ofrecer una opción de búsqueda, donde se solicite al usuario ingresar el nombre de un color e informar si dicho color se encuentra o no en el arreglo.
 */
 
-const int dimFis = 10000000; // Aproximadamente todos los colores del mundo
+const int dimFis = 1000;
 
-void cargarColores(string colores[], int dl)
+void cargarColores(string colores[], int &dl)
 {
-    for(int i = 0; i < dimFis; i++)
+    for (int i = dl; i < dimFis; i++)
     {
         cout << "Ingrese un color ('ff' para finalizar): ";
-        if(colores[i] == "ff")
+        cin >> colores[i];
+
+        if (colores[i] == "ff")
         {
             break;
         }
-        cin >> colores[i];
+
         dl++;
-        cout << endl;
     }
 }
 
 void buscarColor(string colores[], int dl)
 {
     string colorBuscado;
+    bool encontrado = false; // Variable para saber si se encontró el color
+
     cout << "Ingrese el color a buscar: ";
     cin >> colorBuscado;
 
-    for(int i = 0; i < dl; i++)
+    // Búsqueda secuencial
+    for (int i = 0; i < dl; i++)
     {
-        if(colores[i] == colorBuscado)
+        if (colores[i] == colorBuscado)
         {
             cout << "Color " << colorBuscado << " encontrado en la posicion " << i+1 << endl;
+            encontrado = true;
+            break;
         }
-        else
-        {
-            cout << "El color no fue encontrado en el arreglo";
-        }
+    }
+
+    if (!encontrado)
+    {
+        cout << "El color no fue encontrado en el arreglo." << endl;
     }
 }
 
-void menu(string colores[], int dl)
+void menu(string colores[], int &dl)
 {
     char opciones;
     do
     {
-        cout << ""
-    }
+        cout << "Bienvenido al sistema de colores" << endl;
+        cout << "A. Cargar colores" << endl;
+        cout << "B. Buscar color en el arreglo" << endl;
+        cout << "C. Salir" << endl;
+        cout << "Elija una opcion: ";
+        cin >> opciones;
+
+        switch (opciones)
+        {
+            case 'a':
+            case 'A':
+            {
+                cargarColores(colores, dl);
+                break;
+            }
+
+            case 'b':
+            case 'B':
+            {
+                buscarColor(colores, dl);
+                break;
+            }
+
+            case 'c':
+            case 'C':
+            {
+                cout << "Saliendo del sistema..." << endl;
+                break;
+            }
+
+            default:
+            {
+                cout << "Ingrese una opción válida." << endl;
+                break;
+            }
+        }
+    } while (opciones != 'C' && opciones != 'c');
 }
 
 int main()
 {
+    string colores[dimFis]; // Arreglo de colores
+    int dl = 0; // Dimensión lógica inicializada en 0
 
+    menu(colores, dl); // Llama al menú principal
 
     return 0;
 }
