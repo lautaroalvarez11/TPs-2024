a0db2e934411f62b9ef7889c6042be31abf37b13
lautaroalvarez11 <lealvarez485@comunidad.unnoba.edu.ar>
2024-11-04 12:01:46 -0300

    .

--- TP3/Ej8.cpp
+++ TP3/Ej8.cpp
@@ -5,8 +5,8 @@ using namespace std;
 /*
 Implementar una función que reciba dos parámetros: una lista enlazada simple de números enteros y un
 número entero. La función debe eliminar todas las ocurrencias del número indicado en el segundo parámetro.
-Retornar la cantidad de ocurrencias eliminadas. Nota: se debe pasar una sola vez por cada uno de los
-elementos de la lista.
+Retornar la cantidad de ocurrencias eliminadas.
+Nota: Se debe pasar una sola vez por cada uno de los elementos de la lista.
 */
 
 int main()
