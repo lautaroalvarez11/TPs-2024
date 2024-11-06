dc4924776244c77a1120c3938686453013968e2d
lautaroalvarez11 <lealvarez485@comunidad.unnoba.edu.ar>
2024-11-06 12:18:52 -0300

    .

--- TP3/Ej13.cpp
+++ TP3/Ej13.cpp
@@ -53,10 +53,102 @@ void insertarOrdenado(Nodo* &inicio, Nodo* nuevoNodo)
     }
 }
 
+void cargarInformacionSucursal(Nodo*& inicio, int numeroSucursal)
+{
+    Ventas infoVenta;
 
+    cout << "Ingrese el codigo del producto para la sucursal " << numeroSucursal << " (-1 para finalizar): ";
+    cin >> infoVenta.codigo;
+
+    while (infoVenta.codigo != -1)
+    {
+        do
+        {
+            cout << "Ingrese la cantidad de unidades vendidas para la sucursal " << numeroSucursal << ": ";
+            cin >> infoVenta.cantidadVendidas;
+            if (infoVenta.cantidadVendidas < 0)
+            {
+                cout << "Por favor ingrese una cantidad válida (0 o más)" << endl;
+            }
+        } while (infoVenta.cantidadVendidas < 0);
+
+        Nodo* nuevoNodo = crearNodo(infoVenta);
+        insertarOrdenado(inicio, nuevoNodo);
+
+        cout << "Ingrese el codigo del producto para la sucursal " << numeroSucursal << " (-1 para finalizar): ";
+        cin >> infoVenta.codigo;
+    }
+}
+
+Nodo* unirListas(Nodo* lista1, Nodo* lista2)
+{
+    Nodo* listaFusionada = nullptr;
+
+    while (lista1 != nullptr && lista2 != nullptr)
+    {
+        if (lista1 -> dato.codigo < lista2 -> dato.codigo)
+        {
+            Nodo* nuevoNodo = crearNodo(lista1 -> dato);
+            insertarOrdenado(listaFusionada, nuevoNodo);
+            lista1 = lista1 -> siguiente;
+        }
+        else if (lista2 -> dato.codigo < lista1 -> dato.codigo)
+        {
+            Nodo* nuevoNodo = crearNodo(lista2 -> dato);
+            insertarOrdenado(listaFusionada, nuevoNodo);
+            lista2 = lista2 -> siguiente;
+        }
+        else // Si los códigos son iguales, sumar las cantidades
+        {
+            Ventas infoFusionada;
+            infoFusionada.codigo = lista1 -> dato.codigo;
+            infoFusionada.cantidadVendidas = lista1 -> dato.cantidadVendidas + lista2 -> dato.cantidadVendidas;
+            Nodo* nuevoNodo = crearNodo(infoFusionada);
+            insertarOrdenado(listaFusionada, nuevoNodo);
+            lista1 = lista1 -> siguiente;
+            lista2 = lista2 -> siguiente;
+        }
+    }
+
+    // Si quedan nodos en lista1, se agregan a listaFusionada
+    while (lista1 != nullptr)
+    {
+        Nodo* nuevoNodo = crearNodo(lista1 -> dato);
+        insertarOrdenado(listaFusionada, nuevoNodo);
+        lista1 = lista1 -> siguiente;
+    }
+
+    // Si quedan nodos en lista2, se agregan a listaFusionada
+    while (lista2 != nullptr)
+    {
+        Nodo* nuevoNodo = crearNodo(lista2 -> dato);
+        insertarOrdenado(listaFusionada, nuevoNodo);
+        lista2 = lista2 -> siguiente;
+    }
+
+    return listaFusionada;
+}
+
+void imprimirLista(Nodo* lista)
+{
+    while (lista != nullptr)
+    {
+        cout << "Listado de ventas de ambas sucursales:" << endl;
+        cout << "Con el codigo: " << lista -> dato.codigo << "- La cantidad vendida en total es: " << lista -> dato.cantidadVendidas << endl;
+        lista = lista -> siguiente;
+    }
+}
 
 int main()
 {
+    Nodo* lista1 = nullptr;
+    Nodo* lista2 = nullptr;
+
+    cargarInformacionSucursal(lista1, 1);
+    cargarInformacionSucursal(lista2, 2);
+
+    Nodo* listaFusionada = unirListas(lista1, lista2);
+    imprimirLista(listaFusionada);
 
     return 0;
 }
